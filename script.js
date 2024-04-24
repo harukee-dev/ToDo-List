e85e4ea9e4f04e66fc706d812579e9a6018099e2
harukee <hdjemsmc@gmail.com>
2024-04-24 19:52:24 +0300

    todo-list-version4

--- script.js
+++ script.js
@@ -8,12 +8,12 @@ const doneTodosDiv = document.querySelector(".done-todos-div");
 let id = 0;
 
 buttonAddNode.addEventListener("click", function () {
-  if (inputAddNode.value != "") {
+  if (/[a-zа-яё0-9]/i.test(inputAddNode.value)) {
     inputAddNode.placeholder = "";
     id++;
     let div = document.createElement("div");
     div.className = `new-todo div${id}`;
-    div.innerHTML = `<input type="text" maxlength="27" id="${id}" class="input-todo-div " value="${inputAddNode.value}"> <button id="${id}" class="button-delete-js${id} todo-button">
+    div.innerHTML = `<input type="text" placeholder="введите корректное название" maxlength="27" id="${id}" class="input-todo-div " value="${inputAddNode.value}"> <button id="${id}" class="button-delete-js${id} todo-button">
   delete</button><button id="${id}" class="button-done-js${id} todo-button">done</button>`;
     todoDiv.append(div);
     inputAddNode.value = "";
@@ -23,15 +23,33 @@ buttonAddNode.addEventListener("click", function () {
     );
     buttonDoneNode = document.querySelector(`.button-done-js${id}`);
     buttonDoneNode.addEventListener("click", function (event) {
-      doneTodosDiv.innerHTML =
-        doneTodosDiv.innerHTML +
-        `<div class="todo-done-task">· ${
+      if (
+        /[a-zа-яё0-9]/i.test(
           document.getElementById(`${event.target.id}`).value
-        }</div>`;
-      document.querySelector(`.div${event.target.id}`).remove(todoDiv);
+        )
+      ) {
+        doneTodosDiv.innerHTML =
+          doneTodosDiv.innerHTML +
+          `<div class="todo-done-task">· ${
+            document.getElementById(`${event.target.id}`).value
+          }</div>`;
+        document.querySelector(`.div${event.target.id}`).remove(todoDiv);
+      } else {
+        document.getElementById(`${event.target.id}`).value = "";
+      }
     });
+    document
+      .querySelector(".input-todo-div")
+      .addEventListener("focusout", function (event) {
+        if (/[a-zа-яё0-9]/i.test(event.target.value)) {
+          return;
+        } else {
+          event.target.value = "";
+        }
+      });
   } else {
-    inputAddNode.placeholder = "ничего не введено!";
+    inputAddNode.placeholder = "введите корректное название";
+    inputAddNode.value = "";
   }
 });
 
