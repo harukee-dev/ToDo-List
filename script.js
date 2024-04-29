a19acfce1805ff51799961255425cc909f46807d
harukee <hdjemsmc@gmail.com>
2024-04-29 15:26:52 +0300

    todo-list-version7

--- script.js
+++ script.js
@@ -7,14 +7,14 @@ const doneTodosDiv = document.querySelector(".done-todos-div");
 
 let id = 0;
 
-buttonAddNode.addEventListener("click", function () {
+function buttonAddFunc() {
   if (/[a-zа-яё0-9]/i.test(inputAddNode.value)) {
     inputAddNode.placeholder = "";
     id++;
     let div = document.createElement("div");
     div.className = `new-todo div${id}`;
     div.innerHTML = `<input type="text" placeholder="введите корректное название" maxlength="27" id="${id}" class="input-todo-div " value="${inputAddNode.value}"> <button id="${id}" class="button-delete-js${id} todo-button">
-  delete</button><button id="${id}" class="button-done-js${id} todo-button">done</button><button id="${id}" class="todo-button add-subitem-button${id}">+</button>`;
+  X</button><button id="${id}" class="button-done-js${id} todo-button">✓</button><button id="${id}" class="todo-button add-subitem-button${id}">+</button>`;
     todoDiv.append(div);
     inputAddNode.value = "";
     buttonDeleteNode = document.querySelector(`.button-delete-js${id}`);
@@ -28,12 +28,22 @@ buttonAddNode.addEventListener("click", function () {
           document.getElementById(`${event.target.id}`).value
         )
       ) {
-        doneTodosDiv.innerHTML =
-          doneTodosDiv.innerHTML +
-          `<div class="todo-done-task">· ${
-            document.getElementById(`${event.target.id}`).value
-          }</div>`;
-        document.querySelector(`.div${event.target.id}`).remove(todoDiv);
+        if (
+          document.querySelectorAll(`.item-checkbox${event.target.id}`)
+            .length === 0 ||
+          Array.from(
+            document.querySelectorAll(`.item-checkbox${event.target.id}`)
+          ).every((checkbox) => checkbox.checked)
+        ) {
+          doneTodosDiv.innerHTML =
+            doneTodosDiv.innerHTML +
+            `<div class="todo-done-task">· ${
+              document.getElementById(`${event.target.id}`).value
+            }</div>`;
+          document.querySelector(`.div${event.target.id}`).remove(todoDiv);
+        } else {
+          alert("не все пункты выполнены!");
+        }
       } else {
         document.getElementById(`${event.target.id}`).value = "";
       }
@@ -50,14 +60,27 @@ buttonAddNode.addEventListener("click", function () {
     let buttonAddSubitem = document.querySelector(`.add-subitem-button${id}`);
     buttonAddSubitem.addEventListener("click", function (event) {
       let subitemDiv = document.createElement("div");
-      subitemDiv.className = "new-string-subitem";
-      subitemDiv.innerHTML = `<input class="checkbox-subitem" type="checkbox"><input type="text" placeholder="введите подпункт" maxlength="27" class="input-todo-div-subitem" value="Пункт">`;
+      subitemDiv.className = `new-string-subitem subitem-div${event.target.id}`;
+      subitemDiv.innerHTML = `<input class="checkbox-subitem item-checkbox${event.target.id}" type="checkbox"><input type="text" placeholder="введите пункт" maxlength="27" class="input-todo-div-subitem" value="пункт">
+      <button class="delete-subitem-button-style delete-subitem-button${event.target.id}">X</button>`;
       document.querySelector(`.div${event.target.id}`).append(subitemDiv);
+      document
+        .querySelectorAll(`.delete-subitem-button${event.target.id}`)
+        .forEach(function (element) {
+          element.addEventListener("click", (el) =>
+            el.target.parentElement.remove()
+          );
+        });
     });
   } else {
     inputAddNode.placeholder = "введите корректное название";
     inputAddNode.value = "";
   }
-});
+}
 
-// СДЕЛАТЬ АДАПТИВ!!!!!
+buttonAddNode.addEventListener("click", () => buttonAddFunc());
+document.addEventListener("keydown", function (event) {
+  if (event.code === "Enter") {
+    buttonAddNode.click();
+  }
+});
